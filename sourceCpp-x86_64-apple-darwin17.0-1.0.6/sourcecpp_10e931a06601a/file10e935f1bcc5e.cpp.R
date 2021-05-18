3cd5d90538bb0d48955b795651b05d9b08a68823
Xueqing Zou <xz388@nz08.local>
2021-05-18 17:46:50 +0100

    removed classification function

--- sourceCpp-x86_64-apple-darwin17.0-1.0.6/sourcecpp_10e931a06601a/file10e935f1bcc5e.cpp.R
+++
@@ -1,5 +0,0 @@
-`.sourceCpp_1_DLLInfo` <- dyn.load('/Users/xz388/Documents/GitHub/indelsig.tools.lib/sourceCpp-x86_64-apple-darwin17.0-1.0.6/sourcecpp_10e931a06601a/sourceCpp_2.so')
-
-segment <- Rcpp:::sourceCppFunction(function(string, context) {}, FALSE, `.sourceCpp_1_DLLInfo`, 'sourceCpp_1_segment')
-
-rm(`.sourceCpp_1_DLLInfo`)
